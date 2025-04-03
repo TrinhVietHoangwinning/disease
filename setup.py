0e5a3fbd142e671f2e614191afb7823f1798956e
hoangdeptraivcl <tvh180303@gmail.com>
2025-04-03 15:54:07 +0700

    add data ingestion

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Dssease"
-AUTHOR_USER_NAME = "tvh"
+REPO_NAME = "disease"
+AUTHOR_USER_NAME = "TrinhVietHoangwinning"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "tvh180303@gmail.com"
 
